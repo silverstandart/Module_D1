822f8a5d7dd06a05461a70ed25b5694603b1bdd9
silverstandart <81781975+silverstandart@users.noreply.github.com>
2022-04-28 12:22:51 +0300

    Update 0_Iaas_variables.bat

--- IaaS_cloud/0_Iaas_variables.bat
+++ IaaS_cloud/0_Iaas_variables.bat
@@ -4,8 +4,8 @@ rem   1_IaaS_run.bat / IaaS DevOps Module D1.7 / 2022_04_27 / ANa
 rem ---------------------------------------------------------------------------------------
 
 rem ----------------------------- variable definition START -------------------------------
-set AND5_yc_service_account_name=infodba
-set AND5_yc_cloud_id=b1gfdopk51c4d5reva85
-set AND5_yc_folder_id=b1gug0h1o834u3niipmr
+set AND5_yc_service_account_name=<yc service account name>
+set AND5_yc_cloud_id=<yc cloud id>
+set AND5_yc_folder_id=<yc folder id>
 
 rem ----------------------------- variable definition END ---------------------------------
